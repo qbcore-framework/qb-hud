0c57810caf3328a8ca8da53c59f9ba31091d8a54
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-01-18 13:04:27 +0100

    feat(config): use backticks and police stress config

--- config.lua
+++ config.lua
@@ -6,54 +6,55 @@ Config.UseMPH = true -- If true speed math will be done as MPH, if false KPH wil
 Config.MinimumStress = 50 -- Minimum Stress Level For Screen Shaking
 Config.MinimumSpeedUnbuckled = 50 -- Going Over This Speed Will Cause Stress
 Config.MinimumSpeed = 100 -- Going Over This Speed Will Cause Stress
+Config.DisablePoliceStress = true -- If true will disable stress for people with the police job
 
 -- Stress
 
 Config.WhitelistedWeaponArmed = { -- weapons specifically whitelisted to not show armed mode
     -- miscellaneous
-    'weapon_petrolcan',
-    'weapon_hazardcan',
-    'weapon_fireextinguisher',
+    `weapon_petrolcan`,
+    `weapon_hazardcan`,
+    `weapon_fireextinguisher`,
     -- melee
-    'weapon_dagger',
-    'weapon_bat',
-    'weapon_bottle',
-    'weapon_crowbar',
-    'weapon_flashlight',
-    'weapon_golfclub',
-    'weapon_hammer',
-    'weapon_hatchet',
-    'weapon_knuckle',
-    'weapon_knife',
-    'weapon_machete',
-    'weapon_switchblade',
-    'weapon_nightstick',
-    'weapon_wrench',
-    'weapon_battleaxe',
-    'weapon_poolcue',
-    'weapon_briefcase',
-    'weapon_briefcase_02',
-    'weapon_garbagebag',
-    'weapon_handcuffs',
-    'weapon_bread',
-    'weapon_stone_hatchet',
+    `weapon_dagger`,
+    `weapon_bat`,
+    `weapon_bottle`,
+    `weapon_crowbar`,
+    `weapon_flashlight`,
+    `weapon_golfclub`,
+    `weapon_hammer`,
+    `weapon_hatchet`,
+    `weapon_knuckle`,
+    `weapon_knife`,
+    `weapon_machete`,
+    `weapon_switchblade`,
+    `weapon_nightstick`,
+    `weapon_wrench`,
+    `weapon_battleaxe`,
+    `weapon_poolcue`,
+    `weapon_briefcase`,
+    `weapon_briefcase_02`,
+    `weapon_garbagebag`,
+    `weapon_handcuffs`,
+    `weapon_bread`,
+    `weapon_stone_hatchet`,
     -- throwables
-    'weapon_grenade',
-    'weapon_bzgas',
-    'weapon_molotov',
-    'weapon_stickybomb',
-    'weapon_proxmine',
-    'weapon_snowball',
-    'weapon_pipebomb',
-    'weapon_ball',
-    'weapon_smokegrenade',
-    'weapon_flare'
+    `weapon_grenade`,
+    `weapon_bzgas`,
+    `weapon_molotov`,
+    `weapon_stickybomb`,
+    `weapon_proxmine`,
+    `weapon_snowball`,
+    `weapon_pipebomb`,
+    `weapon_ball`,
+    `weapon_smokegrenade`,
+    `weapon_flare`
 }
 
 Config.WhitelistedWeaponStress = {
-    'weapon_petrolcan',
-    'weapon_hazardcan',
-    'weapon_fireextinguisher'
+    `weapon_petrolcan`,
+    `weapon_hazardcan`,
+    `weapon_fireextinguisher`
 }
 
 Config.Intensity = {
