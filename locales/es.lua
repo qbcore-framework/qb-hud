ecb646c3e0f90f39a8366910a60d0c39baf3e268
RDK <99123834+Ridie-Kings@users.noreply.github.com>
2022-02-06 13:36:02 +0100

    Update es.lua
    minor error corrections

--- locales/es.lua
+++ locales/es.lua
@@ -2,20 +2,20 @@ local Translations = {
     notify = {
         ["hud_settings_loaded"] = "Configuración de HUD cargada.",
         ["hud_restart"] = "HUD se está reiniciando.",
-        ["hud_start"] = "HUD ya comenzó.",
+        ["hud_start"] = "HUD funcionando.",
         ["hud_command_info"] = "Este comando restablece la configuración del HUD",
-        ["load_square_map"] = "Cargando el mapa cuadrado...",
+        ["load_square_map"] = "Cargando el mapa cuadrado.",
         ["loaded_square_map"] = "El mapa cuadrado se ha cargado.",
-        ["load_circle_map"] = "Cargando el mapa circulado...",
-        ["loaded_circle_map"] = "El mapa circulado se ha cargado.",
-        ["cinematic_on"] = "Modo de cinematográfica cargado!",
-        ["cinematic_off"] = "Modo de cinematográfico desactivado.",
-        ["engine_on"] = "Motor arrancado!",
+        ["load_circle_map"] = "Cargando el mapa circular.",
+        ["loaded_circle_map"] = "El mapa circular se ha cargado.",
+        ["cinematic_on"] = "Modo cinematográfico activado!",
+        ["cinematic_off"] = "Modo cinematográfico desactivado.",
+        ["engine_on"] = "¡Motor arrancado!",
         ["engine_off"] = "Motor apagado.",
-        ["low_fuel"] = "Nivel de gasolian baja!",
-        ["access_denied"] = "¿Que haces? No estas autorizado!",
-        ["stress_gain"] = "Te estas sintiéndo mas estresado/a.",
-        ["stress_removed"] = "Te estas sintiéndo mas relajado/a."
+        ["low_fuel"] = "¡Nivel de gasolina bajo!",
+        ["access_denied"] = "¿Que haces? ¡No estas autorizado!",
+        ["stress_gain"] = "Te estás sintiéndo más estresado/a.",
+        ["stress_removed"] = "Te estás sintiéndo más relajado/a."
     }
 }
 Lang = Locale:new({phrases = Translations, warnOnMissing = true})
