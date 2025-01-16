e3fcb504c2b8e09edb7513403373f9527dc9f9c5
LawProductions <arturomielan77@gmail.com>
2025-01-16 18:05:42 +0000

    New /togglehud in game command to enable/disable hud.

--- server.lua
+++ server.lua
@@ -1,5 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local ResetStress = false
+local hiddenStates = {} -- Added Variable to track hud toggle state (On/Off)
 
 QBCore.Commands.Add('cash', 'Check Cash Balance', {}, false, function(source, _)
     local Player = QBCore.Functions.GetPlayer(source)
@@ -67,4 +68,29 @@ end)
 
 QBCore.Functions.CreateCallback('hud:server:getMenu', function(_, cb)
     cb(Config.Menu)
+end)
+
+local function ToggleHudForPlayer(source, shouldHide)
+    TriggerClientEvent('qb-hud:client:ToggleHud', source, shouldHide)
+end
+
+exports('ToggleHudForPlayer', ToggleHudForPlayer)
+
+
+QBCore.Commands.Add('togglehud', 'Toggle HUD visibility', {}, false, function(source)
+    hiddenStates[source] = not hiddenStates[source]
+    if hiddenStates[source] then
+        TriggerClientEvent('QBCore:Notify', source, 'HUD Hidden', 'error')
+    else
+        TriggerClientEvent('QBCore:Notify', source, 'HUD Visible', 'success')
+    end
+
+    -- Trigger the client event to actually toggle the HUD
+    TriggerClientEvent('qb-hud:client:ToggleHud', source)
+end)
+
+-- Add player cleanup when they disconnect
+AddEventHandler('playerDropped', function()
+    local source = source
+    hiddenStates[source] = nil
 end)
